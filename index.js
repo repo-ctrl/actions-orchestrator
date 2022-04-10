a6fa16015233ef7417ffed7c574eb413c48d9937
preacherlemon <preacherlemon@outlook.com>
2022-04-10 11:44:03 +0530

    repo_dispatch poc 1

--- index.js
+++ index.js
@@ -2,39 +2,29 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
-// var request = require('request');
+var request = require('request');
+const token = "<TOKEN_GOES_HERE>"
+const caller_repo = "actions-callers"
  
 module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
-
   app.on("issues.opened", async (context) => {
-    //return context.octokit.issues.createComment(issueComment);
-
-    return context.octokit.rest.repos.createDispatchEvent({
-      owner: context.payload.repository.owner.login,
-      repo: "actions-callers",
-      event_type: "call-01",
+    const dispatch_event = "call-01"
+    var options = {
+      'method': 'POST',
+      'url': 'https://api.github.com/repos/'+context.payload.repository.owner.login+'/'+caller_repo+'/dispatches',
+      'headers': {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': 'token '+token,
+        'Content-Type': 'application/json',
+        'user-agent': 'node.js'
+      },
+      body: JSON.stringify({"event_type":dispatch_event})
+    };
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      console.log(response.body);
     });
-
-    // //START
-    // var caller_repo ="actions-callers"
-    // var token = "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"
-    // var options = {
-    //   'method': 'POST',
-    //   'url': 'https://api.github.com/repos/'+context.payload.repository.owner.login+'/'+caller_repo+'/dispatches',
-    //   'headers': {
-    //     'Accept': 'application/vnd.github.v3+json',
-    //     'Authorization': 'token '+token,
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({"event_type":"call-01"})
-    // };
-    // request(options, function (error, response) {
-    //   if (error) throw new Error(error);
-    //   console.log(response.body);
-    // });
-    // // END
+    // END
   });
 
   app.on("pull_request.opened", async (context) => {
@@ -43,11 +33,4 @@ module.exports = (app) => {
     });
     return context.octokit.issues.createComment(issueComment);
   });
-  
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
