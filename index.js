cc1eeb9a09c0611cc08240541cff43b9deba0b8c
preacherlemon <preacherlemon@outlook.com>
2022-04-10 22:56:46 +0530

    adding checks

--- index.js
+++ index.js
@@ -2,68 +2,96 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
-var axios = require('axios');
-const token = ""
+
 const caller_repo = "actions-callers"
 
-function createRepoDispatch(context,caller_repo,event_type,callback) {
-  var data = JSON.stringify({"event_type":event_type});
-  var config = {
-    method: 'post',
-    url: 'https://api.github.com/repos/'+context.payload.repository.owner.login+'/'+caller_repo+'/dispatches',
-    headers: { 
-      'Accept': 'application/vnd.github.v3+json', 
-      'Authorization': 'token '+token, 
-      'Content-Type': 'application/json'
-    },
-    data : data
-  };
-  axios(config).then(function (response) {
-    callback(response);
-  }).catch(function (error) {
-    callback(error);
-  });
-}
- 
-function createCheckRun(context,check_name,callback) {
-  var data = JSON.stringify({"name":check_name, "head_sha":context.payload.pull_request.head.sha});
-  var config = {
-    method: 'post',
-    url: 'https://api.github.com/repos/'+context.payload.repository.full_name+'/check-runs',
-    headers: { 
-      'Accept': 'application/vnd.github.v3+json', 
-      'Authorization': 'token '+token, 
-      'Content-Type': 'application/json'
-    },
-    data : data
-  };
-  axios(config).then(function (response) {
-    callback(response);
-  }).catch(function (error) {
-    callback(error)
-  });
-}
+ module.exports = (app) => {
+  app.on(['pull_request.opened', 'pull_request.synchronize', 'pull_request.reopened', 'pull_request.edited'], async(context) => {
+    runRequiredTests(context, 'pull_request')
+  })
 
-module.exports = (app) => {
-  app.on("pull_request.edited", async (context) => {
-    createCheckRun(context,"This is a test", function (fx_response){
-      if(fx_response.status == 201){
-        createRepoDispatch(context,caller_repo,"call-01",function (fx_response){
-          if(fx_response.status == 204){
-            console.log("Repo Dispatch Successful")
-          } else {
-            //ERRLOG
-            console.log("ERROR: Creating Repo Dispatch failed")
-            console.log(fx_response.response.status)
-            console.log(fx_response.response.data)
-          }
-        });
-      } else {
-        //ERRLOG
-        console.log("ERROR: Creating Check Run failed")
-        console.log(fx_response.response.status)
-        console.log(fx_response.response.data)
-      }
-    }); 
-  });
+  async function runRequiredTests(context, source) {
+    let headSha;
+
+    if (source == "pull_request") {
+      headSha = context.payload.pull_request.head.sha
+    } 
+
+    // Logic to decide the repo dispatches to kick off
+    let requiredTests = await getRequiredTests(context)
+
+    for (let i=0;i<requiredTests.length;i++){
+      let checkRun = await createCheckRun(context, headSha)
+      console.log("DEBUG("+i+"): createCheckRun Complete")
+      //createRepoDispatch
+      let repoDispatch = await createRepoDispatch(context,caller_repo,requiredTests[i])
+      console.log("DEBUG("+i+"): createRepoDispatch Complete")
+
+      //figure out reusable workflow result
+      let result = await makeSuccess(context)
+
+      await resolveCheck(context, headSha, checkRun, result)
+    }
+  }
+
+  async function getRequiredTests(context) {
+    let requiredTests = [];
+    // read YAML to parse the required  
+    requiredTests.push("call-01")
+    return requiredTests
+  }
+
+  async function makeSuccess(context) {
+    return "success"
+  }
+
+  async function createCheckRun(context, headSha) {
+    const startTime = new Date();
+
+    return await context.octokit.checks.create({
+      headers: {
+        accept: "application/vnd.github.v3+json"
+      },
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      name: "Actions Orchestrator",
+      status: "queued",
+      started_at: startTime,
+      head_sha: headSha,
+      output: {
+        title: "Queuing Required Tests",
+        summary: "Required Tests will begin shortly",
+      },
+    })
+  }
+
+  async function resolveCheck(context, headSha, checkRun, result) {
+    
+    await context.octokit.checks.update({
+      headers: {
+        accept: "application/vnd.github.v3+json"
+      },
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      name: "Actions Orchestrator",
+      check_run_id: checkRun.data.id,
+      status: "completed",
+      head_sha: headSha,
+      conclusion: result,
+      output: {
+        title: "Required Tests Complete",
+        summary: "Result is " + result,
+      },
+    })
+  }
+
+  async function createRepoDispatch(context,caller_repo,event_type) {
+    console.log("Owner is "+context.payload.repository.owner.login+" and name is "+caller_repo)
+    return await octokit.rest.repos.createDispatchEvent({
+      owner: context.payload.repository.owner.login,
+      repo: caller_repo,
+      event_type: event_type
+    })
+  }
+
 };
